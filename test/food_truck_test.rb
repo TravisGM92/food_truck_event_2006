6776927f576bbd5040f1ba7a008b3ae540c2e1e9
Travis McKinstry <mckinstrytg@gmail.com>
2020-08-05 09:04:47 -0600

    Create food truck class, pass first test

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -5,6 +5,9 @@ require './lib/food_truck'
 
 class FoodTruckTest <  Minitest::Test
   def test_it_exists
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    assert_instance_of FoodTruck, food_truck
 
   end
 end
