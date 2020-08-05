791a8999c47628a936551212b7370041ca717043
Travis McKinstry <mckinstrytg@gmail.com>
2020-08-05 09:19:47 -0600

    Create test to display name of event

--- test/event_test.rb
+++ test/event_test.rb
@@ -11,4 +11,10 @@ class FoodTruckTest <  Minitest::Test
 
     assert_instance_of Event, event
   end
+
+  def test_it_has_a_name
+    event = Event.new("South Pearl Street Farmers Market")
+
+    assert_equal "South Pearl Street Farmers Market", event.name
+  end
 end
