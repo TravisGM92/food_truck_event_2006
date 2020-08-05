1cb837bef9907167941ebb8c805b8c5dfd2dce13
Travis McKinstry <mckinstrytg@gmail.com>
2020-08-05 09:13:33 -0600

    Create method to add quantity of item (stock an item) to food truck

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -9,6 +9,16 @@ class FoodTruck
   def check_stock(item)
     if inventory[item].nil?
       0
+    else
+      inventory[item]
+    end
+  end
+
+  def stock(item, quantity)
+    if inventory[item].nil?
+      inventory[item] = quantity
+    else
+      inventory[item] += quantity
     end
   end
 
